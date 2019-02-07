5bd9db413b9b552f89907a2944210f2b7b2c8577
unknown <reinaldo.torresrj@gmail.com>
2019-02-07 16:22:26 -0200

    CRUD scaffolding

--- cursoaspnet/Startup.cs
+++ cursoaspnet/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using cursoaspnet.Models;
 
 namespace cursoaspnet {
     public class Startup {
@@ -28,6 +30,9 @@ namespace cursoaspnet {
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+    services.AddDbContext<cursoaspnetContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("cursoaspnetContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
